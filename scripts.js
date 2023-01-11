9daeb6f43ad584383ce3a8b4b276343edf08d8a2
Guilfornari <guilfornari@gmail.com>
2023-01-11 15:52:53 -0300

    Adcionado função que acusa seleção de quantidade de cartas diferente de um número par entre 4 e 14.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 const deckOfCards = [];
-const amountOfCards = prompt("Quantas cartas tu que mané?");
+let amountOfCards = prompt("Quantas cartas tu qué mané?")
 let i = 0;
 let n = 0;
+
+while (amountOfCards != 4
+    && amountOfCards != 6
+    && amountOfCards != 8
+    && amountOfCards != 10
+    && amountOfCards != 12
+    && amountOfCards != 14) {
+    alert("Nunca jogou jogo da memória sua anta? Selecione um número par entre 4 e 14!");
+    amountOfCards = prompt("E agora, quantas cartas vai querer candango?");
+}
 
 document.querySelector(".deck").innerHTML = "";
 
 function addCards() {
-    deckOfCards.push(`<div>carta${i}</div>`)
+    deckOfCards.push(`<div class="carta">carta${i}</div>`)
 }
 
 function shuffle() {
